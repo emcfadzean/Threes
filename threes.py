273c9c738a9ff1e9543825c09fe0c71991830693
emcfadzean <emilio.k.m@gmail.com>
2018-11-01 14:03:47 +1300

    Appears to work, however, it is unacceptably slow.

--- threes.py
+++ threes.py
@@ -7,12 +7,32 @@
 import doctest
 
 candidate = 1;
+y = 1;
+
+#Test for common factors
+def commonFactors(lower, upper):
+    if(lower%2 == 0 and upper%2 == 0):
+        return True;
+    if(upper%lower == 0):
+        return True;
+    for i in range(2, upper):
+        if(upper%i == 0 and lower%i == 0):
+            return True;
+    return False;
+    
+
 #Produce x, y, and z so that z < x < y
-for y in range(1, 100):
-    for x in range(1, y):
-        for z in range(1, x):
-            print("{} {} {} {}".format(candidate, x, y, z));
-            candidate = candidate + 1;
+while(candidate != 70):
+    for x in range(2, y):
+        if(commonFactors(x, y)):
+            break;
+        for z in range(2, x):
+            if(commonFactors(z, x) or commonFactors(z, y)):
+                break;
+            if((x**2+y**2) == (1+z**4)):
+                print("{} {} {} {}".format(candidate, x, y, z));
+                candidate = candidate + 1;
+    y = y + 1;
     
 if __name__ == "__main__":
     doctest.testmod()
